ce56ce90e03b18e83154e83f5b0fd5861f964695
Josh Hight <joshbooks@gmail.com>
2018-08-06 07:30:14 -0700

    initial commit

---
+++ mkJni.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# A script to create a mirror of the class structure
+# in ./jni filled with jni header files
+
+# We assume target/classes is where the .class files live
+
+
+rootDir=`pwd`
+classDir="$rootDir/target/classes"
+
+mkdir jni
+cd jni
+
+jniDir=`pwd`
+
+classFiles=`find $classDir -name "*.class"`
+
+for i in $classFiles;
+do
+	jniLocus=`echo $i | sed -e "s|$classDir|$jniDir|" -e 's|[^/]*.class||'`
+
+	className=`\
+		echo $i | sed \
+		-e "s|$classDir/||" \
+		-e 's|\([^/]*\).class|\1|' \
+		-e 's|/|.|g'`
+
+	echo $className
+	mkdir -p $jniLocus
+	cd $jniLocus
+	javah -cp $classDir $className
+done
+
